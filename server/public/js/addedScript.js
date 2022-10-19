dabb19063a07f623559b26ce8e5c97304da3db0d
WojtekU <voytecu@gmail.com>
2022-10-19 12:55:36 +0200

    zoom modal in progress

--- server/public/js/addedScript.js
+++ server/public/js/addedScript.js
@@ -33,15 +33,15 @@
             fetch(`http://localhost:5000/search-products/${keyword}`)
                 .then(res=>res.json())
                 .then(data=>{ searchDisplay.innerHTML="";
-                    data.map(
-                        product=>searchDisplay.insertAdjacentHTML('beforeend',
+                    data.forEach(
+                        ({name,picture, title})=>{searchDisplay.insertAdjacentHTML('beforeend',
                             `<div style="height: 300px; margin-top: .5%;" class="ml-lg-auto col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="800">
                                 <div class="team-thumb">
-                                    <h5 style="text-align:center">${product.name}</h5>
+                                    <h5 style="text-align:center">${name}</h5>
                                     
                                     <div style="width: 100%; 
                                          height: 200px; 
-                                         background-image: url('${product.picture}');
+                                         background-image: url('${picture}');
                                          background-size:contain;
                                          background-position:center;
                                          background-repeat:no-repeat;
@@ -51,13 +51,13 @@
                                     </div>
 
                                     <ul style="background-image:linear-gradient(90deg,aliceblue,white,aliceblue); text-align: center" class="social-icon mt-3">
-                                        <li><span>${product.title}</span></li>
+                                        <li><span>${title}</span></li>
                                         <li style="cursor:pointer"><a href="#" class="fa fa-paypal" title="Buy now"  data-toggle="modal" data-target="#membershipForm"></a></li>
                                         <li style="cursor:pointer"><a href="#" class="fa fa-shopping-basket" title="Add to basket"></a></li>
                                     </ul>
 
                                 </div>
-                            </div>`)
+                            </div>`) }
                     )
                 })
                 .catch(err=>console.log('Search-fetch error: ',err))
@@ -99,13 +99,32 @@
                     technics2.innerText = item2.technic;
                     img1.src = item1.picture;
                     img2.src = item2.picture;
-
+                    img1.alt = item1.name;
+                    img2.alt = item2.name;
                 })
                 .catch(err=>{
                     console.log('Pictures onload error: ',err.message.slice(-9))
                 });
         }
 
+        function closeZoomModal(modal) {
+            modal.style.width='0';
+            modal.style.height='0';
+            modal.style.left="-100%";
+        }
+
         function zoomImg(element) {
-            console.log(element.tagName);
+
+            const zoomModal = document.getElementById('zoom-modal');
+
+            zoomModal.style.width='100vw';
+            zoomModal.style.height='100vh';
+            zoomModal.style.left='0';
+
+            if(element.tagName==='DIV') {
+                console.log(element.style.backgroundImage.slice(5,-2),element.previousElementSibling.innerText);
+                
+            } else {
+                console.log(element.src, element.alt);
+            }
         }
