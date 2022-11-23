8915b483d948450e61ba363943ad1750844a5004
voytecu314 <voytecu@gmail.com>
2022-11-23 17:55:37 +0100

    session basket imroved

--- server/public/js/added-scripts/displayBasket.js
+++ server/public/js/added-scripts/displayBasket.js
@@ -15,6 +15,7 @@ const basket_elements = {
 }
 
 function displayBasket(x) {
+    
     if(checkJWT(x)?.user_auth){
 
         const sessionBasket = sessionStorage.getItem('W-H-A-Session-Basket');
@@ -32,18 +33,27 @@ function displayBasket(x) {
             fetch('http://localhost:5000/basket/get-session-basket',fetchOptions)
             .then(res=>res.json())
             .then(({sessionBasket})=>{
+
+                const calculateBasketPrice = (sessionBasket) => document.getElementById('total-price').innerText=`${sessionBasket.reduce((sum,item)=>sum+item.price,0).toFixed(2)}€`;
+                const calculateTotalDiscount = (sessionBasket) => document.getElementById('total-price-after-discount').innerText=`${sessionBasket.reduce((sum,item)=>{
+                    const itemDiscountPrice = item.discount.set?(item.price-item.price*(item.discount.percent/100)):item.price;
+                    return sum+itemDiscountPrice},0).toFixed(2)}€`;
+
                 const basketTable = document.getElementById('basket-table');
-                sessionBasket.map(item=>basketTable.insertAdjacentHTML('beforeend',
+                sessionBasket.map((item,i)=>basketTable.insertAdjacentHTML('beforeend',
                     `<tr>
                         <td>${item.name} - ${item.title}</td>
-                        <td>${item.price}€</td>
-                        <td>${item.discount.set?item.discount.percent:0}%</td>
-                        <td>
-                            <a href='#' id="${item._id}" onclick="(()=>{sessionStorage.setItem('W-H-A-Session-Basket',JSON.stringify(JSON.parse(sessionStorage.getItem('W-H-A-Session-Basket')).filter(id=>id!=this.id)));this.parentElement.parentElement.remove()})(this)">
+                        <td  class="text-right">${item.price}€</td>
+                        <td  class="text-right">${item.discount.set?item.discount.percent:0}%</td>
+                        <td  class="text-center">
+                            <a href='#' id="${item._id}" onclick="(()=>{const sessionBasket = JSON.parse(sessionStorage.getItem('W-H-A-Session-Basket')).filter(id=>id!=this.id); sessionStorage.setItem('W-H-A-Session-Basket',JSON.stringify(sessionBasket));this.parentElement.parentElement.remove(); displayBasket(this) })(this)">
                                 <i class="fa fa-close"></i>
                             </a></td>
                     </tr>`));
-            })
+                
+                    calculateBasketPrice(sessionBasket);
+                    calculateTotalDiscount(sessionBasket);
+                })
             .catch(err=>console.log('Session basket fetch error',err.message));
 
         }
